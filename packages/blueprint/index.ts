f0e9d0751cc3fb28f6ee133aeefed5285d27f147
Eric Wooley <ericwooley@gmail.com>
2017-08-06 11:27:53 -0700

    added inquirer

--- packages/blueprint/index.ts
+++ packages/blueprint/index.ts
@@ -1,7 +1,20 @@
 import * as upResolve from "@tww/up-resolve";
+import * as inquirer from "inquirer";
 import * as shelljs from "shelljs";
-const blueprintFolders = upResolve([".blueprints"]);
-const blueprints = blueprintFolders
-  .map(blueprintFolder => shelljs.ls(blueprintFolder))
-  .reduce((total, folders) => [...total, ...folders], []);
-console.log("available blueprints", blueprints, blueprintFolders);
+async function run() {
+  const blueprintFolders = upResolve([".blueprints"]);
+  const blueprints = blueprintFolders
+    .map(blueprintFolder => shelljs.ls(blueprintFolder))
+    .reduce((total, folders) => [...total, ...folders], []);
+  const answer = await inquirer.prompt([
+    {
+      choices: blueprints,
+      message: "select blueprint",
+      name: "blueprint",
+      type: "list"
+    }
+  ]);
+  console.log("selected answer", answer.blueprint);
+}
+
+run();
